05285d83991862dc079eab4acdc29084cd80cff7
Anthonny Tineo Ruiz <anthonnytineo@MacBook-Pro-de-Anthonny.local>
2021-05-20 18:56:28 -0400

    inicio de los estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
 import './App.css';
 
 function App() {
+
+const[persona, setpersona] = useState( {
+  nombre:"",
+  primerApellido:"",
+  segundoApellido:"",
+});
+
+const manejarFormulario = (event) => {
+  const {name, value} = event.target;
+  setpersona(prevPersona => ({
+    ...prevPersona,
+    [name]: value
+  }))
+}
+
+const submit = (event) => {
+  event.preventDeFault();
+  console.log(persona); 
+}
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onsubmit={submit}>
+        <div>
+          <label className="label" htmlFor="">Nombre:</label>
+          <input className="input" type="text" value={persona.nombre} name="nombre" onChange={manejarFormulario} />
+        </div>
+        <div>
+          <label className="label" htmlFor="">Primer Apellido:</label>
+          <input className="input" type="text" value={persona.primerApellido} name="primerApellido" onChange={manejarFormulario} />
+        </div>
+        <div>
+          <label className="label" htmlFor="">Segundo Apellido:</label>
+          <input className="input" type="text" value={persona.segundoApellido} name="segundoApellido" onChange={manejarFormulario} />
+        </div>
+        <button className="submit">Submit</button>
+      </form>
     </div>
   );
 }
